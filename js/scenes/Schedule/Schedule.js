6da837814172bec8d664c411f20f2bc866e19de3
Jonathan Funk <jonfunk88@hotmail.com>
2017-03-01 16:56:06 -0800

    styled schedule scene

--- js/scenes/Schedule/Schedule.js
+++ js/scenes/Schedule/Schedule.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { 
-  ListView, 
-  View, 
+import {
+  ListView,
+  View,
   Text,
-  StyleSheet 
+  StyleSheet
 } from 'react-native';
-import { typography, colors } from './../../config/styles'
+import { typography, colors } from './../../config/styles';
 
 const time = (timestamp) => {
   let d = new Date(timestamp * 1000);
-	let hh = d.getHours();
-	let	h = hh;
-	let min = ('0' + d.getMinutes()).slice(-2);
-	let	ampm = 'AM';
-  
-	if (hh > 12) {
-		h = hh - 12;
-		ampm = 'PM';
-	} else if (hh === 12) {
-		h = 12;
-		ampm = 'PM';
-	} else if (hh == 0) {
-		h = 12;
-	}
+  let hh = d.getHours();
+  let h = hh;
+  let min = ('0' + d.getMinutes()).slice(-2);
+  let ampm = 'AM';
+
+  if (hh > 12) {
+    h = hh - 12;
+    ampm = 'PM';
+  } else if (hh === 12) {
+    h = 12;
+    ampm = 'PM';
+  } else if (hh == 0) {
+    h = 12;
+  }
   return h + ':' + min + ' ' + ampm;
 }
 
@@ -31,16 +31,16 @@ const Schedule = ({data}) => {
   return (
     <ListView
       dataSource={data}
-      renderSectionHeader={(sectionData, startTime) => 
-        <View>
-          <Text>{time(startTime)}</Text>
+      renderSectionHeader={(sectionData, startTime) =>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>{time(startTime)}</Text>
         </View>
       }
-      
-      renderRow={(data) => 
-        <View>
-          <Text>{data.title}</Text>
-          <Text>{data.location}</Text>
+
+      renderRow={(data) =>
+        <View style={styles.row}>
+          <Text style={styles.title}>{data.title}</Text>
+          <Text style={styles.location}>{data.location}</Text>
         </View>
       }
     />
@@ -48,10 +48,30 @@ const Schedule = ({data}) => {
 }
 
 const styles = StyleSheet.create({
-  separator: {
-    flex: 1,
-    height: 1,
-    backgroundColor: colors.mediumGrey,
+  header: {
+    height: 25,
+    backgroundColor: colors.lightGrey,
+    paddingLeft: 10,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerText: {
+    fontFamily: typography.fontMain,
+    fontSize: 12,
+  },
+  row: {
+    padding: 10,
+    borderBottomColor: colors.lightGrey,
+    borderBottomWidth: 1,
+  },
+  title: {
+    fontFamily: typography.fontMain,
+    marginBottom: 5,
+  },
+  location: {
+    fontFamily: typography.fontMainLight,
+    fontSize: 12,
+    color: colors.mediumGrey,
   },
 });
 
